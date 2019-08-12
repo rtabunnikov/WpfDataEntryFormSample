559863f6d57431fcb1285565e19a9a0836a36804
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2019-08-12 14:56:35 +0300

    Sample payroll data

--- WpfDataEntryFormSample/MainWindow.xaml.cs
+++ WpfDataEntryFormSample/MainWindow.xaml.cs
@@ -19,14 +19,114 @@ namespace WpfDataEntryFormSample {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : ThemedWindow {
+        readonly List<PayrollModel> payrollData = new List<PayrollModel>();
+
         public MainWindow() {
             InitializeComponent();
+            InitializePayrollData();
             LoadDocumentTemplate();
         }
 
         private void LoadDocumentTemplate() {
             spreadsheetControl1.LoadDocument("PayrollCalculatorTemplate.xlsx");
             spreadsheetControl1.Document.History.IsEnabled = false;
+        }
+
+        private void InitializePayrollData() {
+            payrollData.Add(new PayrollModel() {
+                EmployeeName = "Linda Brown",
+                HourlyWages = 10.0,
+                RegularHoursWorked = 40,
+                VacationHours = 5,
+                SickHours = 1,
+                OvertimeHours = 0,
+                OvertimeRate = 15.0,
+                OtherDeduction = 20.0,
+                TaxStatus = 1,
+                FederalAllowance = 4,
+                StateTax = 0.023,
+                FederalIncomeTax = 0.28,
+                SocialSecurityTax = 0.063,
+                MedicareTax = 0.0145,
+                InsuranceDeduction = 20.0,
+                OtherRegularDeduction = 40.0
+            });
+
+            payrollData.Add(new PayrollModel() {
+                EmployeeName = "Kate Smith",
+                HourlyWages = 11.0,
+                RegularHoursWorked = 45,
+                VacationHours = 5,
+                SickHours = 0,
+                OvertimeHours = 3,
+                OvertimeRate = 20.0,
+                OtherDeduction = 20.0,
+                TaxStatus = 1,
+                FederalAllowance = 4,
+                StateTax = 0.0245,
+                FederalIncomeTax = 0.276,
+                SocialSecurityTax = 0.061,
+                MedicareTax = 0.015,
+                InsuranceDeduction = 20.0,
+                OtherRegularDeduction = 42.0
+            });
+
+            payrollData.Add(new PayrollModel() {
+                EmployeeName = "Nick Taylor",
+                HourlyWages = 15.0,
+                RegularHoursWorked = 40,
+                VacationHours = 6,
+                SickHours = 2,
+                OvertimeHours = 6,
+                OvertimeRate = 40.0,
+                OtherDeduction = 21.0,
+                TaxStatus = 2,
+                FederalAllowance = 3,
+                StateTax = 0.0301,
+                FederalIncomeTax = 0.2702,
+                SocialSecurityTax = 0.068,
+                MedicareTax = 0.015,
+                InsuranceDeduction = 22.0,
+                OtherRegularDeduction = 39.0
+            });
+
+            payrollData.Add(new PayrollModel() {
+                EmployeeName = "Tommy Dickson",
+                HourlyWages = 20.0,
+                RegularHoursWorked = 40,
+                VacationHours = 0,
+                SickHours = 0,
+                OvertimeHours = 3,
+                OvertimeRate = 45.0,
+                OtherDeduction = 12.46,
+                TaxStatus = 3,
+                FederalAllowance = 4,
+                StateTax = 0.045,
+                FederalIncomeTax = 0.2904,
+                SocialSecurityTax = 0.084,
+                MedicareTax = 0.0143,
+                InsuranceDeduction = 41.4,
+                OtherRegularDeduction = 24.3
+            });
+
+            payrollData.Add(new PayrollModel() {
+                EmployeeName = "Emmy Milton",
+                HourlyWages = 32.0,
+                RegularHoursWorked = 45,
+                VacationHours = 0,
+                SickHours = 0,
+                OvertimeHours = 5,
+                OvertimeRate = 40.0,
+                OtherDeduction = 0.0,
+                TaxStatus = 2,
+                FederalAllowance = 3,
+                StateTax = 0.025,
+                FederalIncomeTax = 0.28,
+                SocialSecurityTax = 0.064,
+                MedicareTax = 0.0143,
+                InsuranceDeduction = 19.34,
+                OtherRegularDeduction = 25.0
+            });
         }
     }
 }
---
+++ WpfDataEntryFormSample/PayrollModel.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataEntryFormSample {
+    /// <summary>
+    /// Entity class with represents payroll item (record) 
+    /// </summary>
+    public class PayrollModel : INotifyPropertyChanged {
+        private string employeeName;
+        private double hourlyWages;
+        private double regularHoursWorked;
+        private double vacationHours;
+        private double sickHours;
+        private double overtimeHours;
+        private double overtimeRate;
+        private double otherDeduction;
+        private int taxStatus;
+        private int federalAllowance;
+        private double stateTax;
+        private double federalIncomeTax;
+        private double socialSecurityTax;
+        private double medicareTax;
+        private double insuranceDeduction;
+        private double otherRegularDeduction;
+
+        public string EmployeeName {
+            get => employeeName;
+            set {
+                if (employeeName != value) {
+                    employeeName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double HourlyWages {
+            get => hourlyWages;
+            set {
+                if (hourlyWages != value) {
+                    hourlyWages = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double RegularHoursWorked {
+            get => regularHoursWorked;
+            set {
+                if (regularHoursWorked != value) {
+                    regularHoursWorked = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double VacationHours {
+            get => vacationHours;
+            set {
+                if (vacationHours != value) {
+                    vacationHours = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double SickHours {
+            get => sickHours;
+            set {
+                if (sickHours != value) {
+                    sickHours = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double OvertimeHours {
+            get => overtimeHours;
+            set {
+                if (overtimeHours != value) {
+                    overtimeHours = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double OvertimeRate {
+            get => overtimeRate;
+            set {
+                if (overtimeRate != value) {
+                    overtimeRate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double OtherDeduction {
+            get => otherDeduction;
+            set {
+                if (otherDeduction != value) {
+                    otherDeduction = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int TaxStatus {
+            get => taxStatus;
+            set {
+                if (taxStatus != value) {
+                    taxStatus = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int FederalAllowance {
+            get => federalAllowance;
+            set {
+                if (federalAllowance != value) {
+                    federalAllowance = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double StateTax {
+            get => stateTax;
+            set {
+                if (stateTax != value) {
+                    stateTax = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double FederalIncomeTax {
+            get => federalIncomeTax;
+            set {
+                if (federalIncomeTax != value) {
+                    federalIncomeTax = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double SocialSecurityTax {
+            get => socialSecurityTax;
+            set {
+                if (socialSecurityTax != value) {
+                    socialSecurityTax = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double MedicareTax {
+            get => medicareTax;
+            set {
+                if (medicareTax != value) {
+                    medicareTax = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double InsuranceDeduction {
+            get => insuranceDeduction;
+            set {
+                if (insuranceDeduction != value) {
+                    insuranceDeduction = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public double OtherRegularDeduction {
+            get => otherRegularDeduction;
+            set {
+                if (otherRegularDeduction != value) {
+                    otherRegularDeduction = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
