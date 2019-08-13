b40bc361d96c8c8303006d74170c532c18165a47
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2019-08-13 15:21:14 +0300

    Data navigator

--- WpfDataEntryFormSample/MainWindow.xaml.cs
+++ WpfDataEntryFormSample/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using DevExpress.Xpf.Core;
 using DevExpress.Xpf.Editors.Settings;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,15 +22,17 @@ namespace WpfDataEntryFormSample {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : ThemedWindow {
-        readonly List<PayrollModel> payrollData = new List<PayrollModel>();
+        private readonly PayrollViewModel payrollViewModel;
 
         public MainWindow() {
             InitializeComponent();
-            InitializePayrollData();
             LoadDocumentTemplate();
             BindCustomEditors();
+            CollectionViewSource payrollViewSource = (CollectionViewSource)FindResource("PayrollViewSource");
+            payrollViewModel = new PayrollViewModel(payrollViewSource.View);
+            DataContext = payrollViewModel;
         }
-
+        
         private void LoadDocumentTemplate() {
             spreadsheetControl1.LoadDocument("PayrollCalculatorTemplate.xlsx");
             spreadsheetControl1.Document.History.IsEnabled = false;
@@ -79,103 +82,6 @@ namespace WpfDataEntryFormSample {
                 if (editors.Count == 1)
                     spreadsheetControl1.OpenCellEditor(DevExpress.XtraSpreadsheet.CellEditorMode.Edit);
             }
-        }
-
-        private void InitializePayrollData() {
-            payrollData.Add(new PayrollModel() {
-                EmployeeName = "Linda Brown",
-                HourlyWages = 10.0,
-                RegularHoursWorked = 40,
-                VacationHours = 5,
-                SickHours = 1,
-                OvertimeHours = 0,
-                OvertimeRate = 15.0,
-                OtherDeduction = 20.0,
-                TaxStatus = 1,
-                FederalAllowance = 4,
-                StateTax = 0.023,
-                FederalIncomeTax = 0.28,
-                SocialSecurityTax = 0.063,
-                MedicareTax = 0.0145,
-                InsuranceDeduction = 20.0,
-                OtherRegularDeduction = 40.0
-            });
-
-            payrollData.Add(new PayrollModel() {
-                EmployeeName = "Kate Smith",
-                HourlyWages = 11.0,
-                RegularHoursWorked = 45,
-                VacationHours = 5,
-                SickHours = 0,
-                OvertimeHours = 3,
-                OvertimeRate = 20.0,
-                OtherDeduction = 20.0,
-                TaxStatus = 1,
-                FederalAllowance = 4,
-                StateTax = 0.0245,
-                FederalIncomeTax = 0.276,
-                SocialSecurityTax = 0.061,
-                MedicareTax = 0.015,
-                InsuranceDeduction = 20.0,
-                OtherRegularDeduction = 42.0
-            });
-
-            payrollData.Add(new PayrollModel() {
-                EmployeeName = "Nick Taylor",
-                HourlyWages = 15.0,
-                RegularHoursWorked = 40,
-                VacationHours = 6,
-                SickHours = 2,
-                OvertimeHours = 6,
-                OvertimeRate = 40.0,
-                OtherDeduction = 21.0,
-                TaxStatus = 2,
-                FederalAllowance = 3,
-                StateTax = 0.0301,
-                FederalIncomeTax = 0.2702,
-                SocialSecurityTax = 0.068,
-                MedicareTax = 0.015,
-                InsuranceDeduction = 22.0,
-                OtherRegularDeduction = 39.0
-            });
-
-            payrollData.Add(new PayrollModel() {
-                EmployeeName = "Tommy Dickson",
-                HourlyWages = 20.0,
-                RegularHoursWorked = 40,
-                VacationHours = 0,
-                SickHours = 0,
-                OvertimeHours = 3,
-                OvertimeRate = 45.0,
-                OtherDeduction = 12.46,
-                TaxStatus = 3,
-                FederalAllowance = 4,
-                StateTax = 0.045,
-                FederalIncomeTax = 0.2904,
-                SocialSecurityTax = 0.084,
-                MedicareTax = 0.0143,
-                InsuranceDeduction = 41.4,
-                OtherRegularDeduction = 24.3
-            });
-
-            payrollData.Add(new PayrollModel() {
-                EmployeeName = "Emmy Milton",
-                HourlyWages = 32.0,
-                RegularHoursWorked = 45,
-                VacationHours = 0,
-                SickHours = 0,
-                OvertimeHours = 5,
-                OvertimeRate = 40.0,
-                OtherDeduction = 0.0,
-                TaxStatus = 2,
-                FederalAllowance = 3,
-                StateTax = 0.025,
-                FederalIncomeTax = 0.28,
-                SocialSecurityTax = 0.064,
-                MedicareTax = 0.0143,
-                InsuranceDeduction = 19.34,
-                OtherRegularDeduction = 25.0
-            });
         }
     }
 }
---
+++ WpfDataEntryFormSample/PayrollViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataEntryFormSample {
+    public class PayrollViewModel : INotifyPropertyChanged {
+        
+        public PayrollViewModel(ICollectionView payroll) {
+            Payroll = payroll ?? throw new ArgumentNullException("payroll");
+            Payroll.CurrentChanged += Payroll_CurrentChanged;
+        }
+
+        public ICollectionView Payroll { get; }
+
+        public int Count => Payroll.Cast<object>().Count();
+
+        public void MoveFirst() => Payroll.MoveCurrentToFirst();
+        public void MovePrevious() => Payroll.MoveCurrentToPrevious();
+        public void MoveNext() => Payroll.MoveCurrentToNext();
+        public void MoveLast() => Payroll.MoveCurrentToLast();
+
+        public bool CanMovePrevious() => Payroll.CurrentPosition > 0;
+        public bool CanMoveNext() => Payroll.CurrentPosition < Count - 1;
+
+        public string DisplayText => !Payroll.IsEmpty ? $"Record {Payroll.CurrentPosition + 1} of {Count}" : string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Payroll_CurrentChanged(object sender, EventArgs e) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisplayText"));
+    }
+}
